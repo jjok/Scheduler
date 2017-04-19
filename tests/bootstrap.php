ad3a2584b0fdd752d9e449bffb6569d5d42945fe
Jonathan Jefferies <jonathan@jjok.co.uk>
2017-04-19 23:47:46 +0100

    Try to fix test bootstrap again

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 spl_autoload_register(function(string $class) {
-    $file = sprintf('src/%s.php', str_replace('\\', '/', $class));
+    $file = sprintf('src/%s.php', str_replace(['jjok\Scheduler\\', '\\'], ['', '/'], $class));
 
     if(file_exists($file)) {
         require_once $file;
