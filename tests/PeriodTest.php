07a7f8586297322b605153adb370274b888c7a2f
Jonathan Jefferies <jonathan@jjok.co.uk>
2017-05-02 22:41:07 +0100

    Refactor PeriodTest

--- tests/PeriodTest.php
+++ tests/PeriodTest.php
@@ -21,25 +21,35 @@ final class PeriodTest extends TestCase
     public function testPeriodDoesNotIncludeTimesBeforeStart() {
         $period = new Period(Time::fromString('08:00:00'), Time::fromString('09:00:00'));
 
-        $this->assertFalse($period->includes(Time::fromString('00:00:00')));
-        $this->assertFalse($period->includes(Time::fromString('04:34:10')));
-        $this->assertFalse($period->includes(Time::fromString('07:59:59')));
+        $this->assertPeriodDoesNotIncludeTime($period, '00:00:00');
+        $this->assertPeriodDoesNotIncludeTime($period, '04:34:10');
+        $this->assertPeriodDoesNotIncludeTime($period, '07:59:59');
     }
 
     public function testPeriodIncludesTimesFromStartToEnd() {
         $period = new Period(Time::fromString('08:00:00'), Time::fromString('09:00:00'));
 
-        $this->assertTrue($period->includes(Time::fromString('08:00:00')));
-        $this->assertTrue($period->includes(Time::fromString('08:34:01')));
-        $this->assertTrue($period->includes(Time::fromString('08:59:59')));
-        $this->assertTrue($period->includes(Time::fromString('09:00:00')));
+        $this->assertPeriodIncludesTime($period, '08:00:00');
+        $this->assertPeriodIncludesTime($period, '08:34:01');
+        $this->assertPeriodIncludesTime($period, '08:59:59');
+        $this->assertPeriodIncludesTime($period, '09:00:00');
     }
 
     public function testPeriodDoesNotIncludeTimesAfterEnd() {
         $period = new Period(Time::fromString('08:00:00'), Time::fromString('09:00:00'));
 
-        $this->assertFalse($period->includes(Time::fromString('09:00:01')));
-        $this->assertFalse($period->includes(Time::fromString('12:00:00')));
-        $this->assertFalse($period->includes(Time::fromString('23:59:59')));
+        $this->assertPeriodDoesNotIncludeTime($period, '09:00:01');
+        $this->assertPeriodDoesNotIncludeTime($period, '12:00:00');
+        $this->assertPeriodDoesNotIncludeTime($period, '23:59:59');
+    }
+
+    private function assertPeriodIncludesTime(Period $period, string $time)
+    {
+        $this->assertTrue($period->includes(Time::fromString($time)));
+    }
+
+    private function assertPeriodDoesNotIncludeTime(Period $period, string $time)
+    {
+        $this->assertFalse($period->includes(Time::fromString($time)));
     }
 }
